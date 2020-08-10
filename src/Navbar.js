d80a44197d4704538b6e2c667954043d273c90d9
Danilo <realdanilo@users.noreply.github.com>
2020-08-10 08:26:37 -0700

    Add palette footer

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,68 +1,83 @@
-import React, { Component } from 'react'
-import Slider, { Range } from 'rc-slider'
-import 'rc-slider/assets/index.css'
-import './Navbar.css'
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import Snackbar from '@material-ui/core/Snackbar';
-import CloseIcon from '@material-ui/icons/Close';
-import IconButton from '@material-ui/core/IconButton';
+import React, { Component } from "react";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+import "./Navbar.css";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
+import IconButton from "@material-ui/core/IconButton";
 
 class Navbar extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            format: 'hex',
-            open: false
-        }
-        this.handleFormatChange = this.handleFormatChange.bind(this)
-        this.closeSnackbar = this.closeSnackbar.bind(this)
-    }
-    handleFormatChange(e) {
-        this.setState({ format: e.target.value, open: true }, () => {
-            this.props.handleFormat(this.state.format)
-            setTimeout(() => this.setState({ open: false }), 5000)
-        })
-
-    }
-    closeSnackbar() {
-        this.setState({ open: false })
-    }
-    render() {
-        const { slider, changeSlider } = this.props
-        const { format, open } = this.state
-        return (
-            <nav className="Navbar">
-                <div className="left-navbar">
-                    <div className="logo">
-                        <a href="#">React Color Picker</a>
-                    </div>
-                    <div className="slider">
-                        <span>Level {slider}</span>
-                        <Slider className="slider-nav" defaultValue={slider} min={100} max={900} onAfterChange={changeSlider} step={100} />
-
-                    </div>
-                </div>
-                <div className="right-navbar">
-                    <Select onChange={this.handleFormatChange} value={format}>
-                        <MenuItem value="hex">HEX - #FFFFFF</MenuItem>
-                        <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
-                        <MenuItem value="rgba">RGBA - rgba(255,255,255,1)</MenuItem>
-                    </Select>
-                </div>
-                <Snackbar anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-                    open={open}
-                    autoHideDuration={3000}
-                    message={<span id="message-id">{`Format Changed to ${format.toUpperCase()}`}</span>}
-                    ContentProps={{
-                        "aria-describedby": "message-id"
-                    }}
-                    action={[<IconButton onClick={this.closeSnackbar} key="close" aria-label="close"><CloseIcon /></IconButton>]}
-                    onClose={this.closeSnackbar}
-                />
-            </nav>
-
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      format: "hex",
+      open: false,
+    };
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.closeSnackbar = this.closeSnackbar.bind(this);
+  }
+  handleFormatChange(e) {
+    this.setState({ format: e.target.value, open: true }, () => {
+      this.props.handleFormat(this.state.format);
+    });
+  }
+  closeSnackbar() {
+    this.setState({ open: false });
+  }
+  render() {
+    const { slider, changeSlider } = this.props;
+    const { format, open } = this.state;
+    return (
+      <nav className="Navbar">
+        <div className="left-navbar">
+          <div className="logo">
+            <a href="#">React Color Picker</a>
+          </div>
+          <div className="slider">
+            <span>Level {slider}</span>
+            <Slider
+              className="slider-nav"
+              defaultValue={slider}
+              min={100}
+              max={900}
+              onAfterChange={changeSlider}
+              step={100}
+            />
+          </div>
+        </div>
+        <div className="right-navbar">
+          <Select onChange={this.handleFormatChange} value={format}>
+            <MenuItem value="hex">HEX - #FFFFFF</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255,1)</MenuItem>
+          </Select>
+        </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={open}
+          autoHideDuration={3000}
+          onClose={this.closeSnackbar}
+          message={
+            <span id="message-id">{`Format Changed to ${format.toUpperCase()}`}</span>
+          }
+          ContentProps={{
+            "aria-describedby": "message-id",
+          }}
+          action={[
+            <IconButton
+              onClick={this.closeSnackbar}
+              key="close"
+              aria-label="close"
+              color="inherit"
+            >
+              <CloseIcon />
+            </IconButton>,
+          ]}
+        />
+      </nav>
+    );
+  }
 }
-export default Navbar
+export default Navbar;
--- src/Palette.js
+++ src/Palette.js
@@ -1,39 +1,48 @@
-import React, { Component } from 'react'
-import ColorBox from './ColorBox'
-import Navbar from './Navbar'
-import './Palette.css'
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import Navbar from "./Navbar";
+import "./Palette.css";
 class Palette extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            slider: 500,
-            format: 'hex'
-        }
-        this.changeSlider = this.changeSlider.bind(this)
-        this.handleFormat = this.handleFormat.bind(this)
-    }
-    changeSlider(newSlider) {
-        this.setState({ slider: newSlider })
-    }
-    handleFormat(value) {
-        this.setState({ format: value })
-    }
-    render() {
-        const { colors } = this.props.palette
-        const { slider, format } = this.state
-        const colorBoxes = colors[slider].map((color, i) => <ColorBox key={i} background={color[format]} name={color.name} />)
-        return (
-            <div className="Palette">
-                {/* Navbar */}
-                <Navbar className="navbar" slider={slider} changeSlider={this.changeSlider} handleFormat={this.handleFormat} />
-                {/* Color Boxes */}
-                <div className="Palette-colors">
-                    {colorBoxes}
-                </div>
-                {/* Footer */}
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      slider: 500,
+      format: "hex",
+    };
+    this.changeSlider = this.changeSlider.bind(this);
+    this.handleFormat = this.handleFormat.bind(this);
+  }
+  changeSlider(newSlider) {
+    this.setState({ slider: newSlider });
+  }
+  handleFormat(value) {
+    this.setState({ format: value });
+  }
+  render() {
+    const { colors, paletteName, emoji } = this.props.palette;
+    const { slider, format } = this.state;
+    const colorBoxes = colors[slider].map((color, i) => (
+      <ColorBox key={i} background={color[format]} name={color.name} />
+    ));
+    return (
+      <div className="Palette">
+        {/* Navbar */}
+        <Navbar
+          className="navbar"
+          slider={slider}
+          changeSlider={this.changeSlider}
+          handleFormat={this.handleFormat}
+        />
+        {/* Color Boxes */}
+        <div className="Palette-colors">{colorBoxes}</div>
+        {/* Footer */}
+        <footer className="Palette-footer">
+          <p>{paletteName}</p>
+          <span className="emoji">{emoji}</span>
+        </footer>
+      </div>
+    );
+  }
 }
 
-export default Palette
+export default Palette;
