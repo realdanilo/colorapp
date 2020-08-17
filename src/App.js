3f827df35d0bf0642345885857267f159c7bf6cb
Danilo <realdanilo@users.noreply.github.com>
2020-08-16 17:44:22 -0700

    first version of animated routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { Route, Switch } from "react-router-dom";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
 import NewPaletteForm from "./NewPaletteForm";
+//animation
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 class App extends Component {
   constructor(props) {
@@ -46,49 +48,67 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={(rp) => (
-              <PaletteList
-                palettes={this.state.palettes}
-                {...rp}
-                handleDelete={this.deletePalette}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/palette/new"
-            render={(rp) => (
-              <NewPaletteForm
-                {...rp}
-                savePalette={this.savePalette}
-                palettes={this.state.palettes}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/palette/:id"
-            render={(rp) => (
-              <Palette palette={this.findPalette(rp.match.params.id)} />
-            )}
-          />
-          <Route
-            exact
-            path="/palette/:paletteId/:colorId"
-            render={(rp) => (
-              <SingleColorPalette
-                {...rp}
-                colorId={rp.match.params.colorId}
-                palette={this.findPalette(rp.match.params.paletteId)}
-              />
-            )}
-          />
-          <Route render={() => <h1>Not found</h1>} />
-        </Switch>
+        <Route
+          render={({ location }) => (
+            <TransitionGroup>
+              <CSSTransition key={location.key} classNames="fade" timeout={500}>
+                <Switch location={location}>
+                  <Route
+                    exact
+                    path="/"
+                    render={(rp) => (
+                      <div className="page">
+                        <PaletteList
+                          palettes={this.state.palettes}
+                          {...rp}
+                          handleDelete={this.deletePalette}
+                        />
+                      </div>
+                    )}
+                  />
+                  <Route
+                    exact
+                    path="/palette/new"
+                    render={(rp) => (
+                      <div className="page">
+                        <NewPaletteForm
+                          {...rp}
+                          savePalette={this.savePalette}
+                          palettes={this.state.palettes}
+                        />
+                      </div>
+                    )}
+                  />
+                  <Route
+                    exact
+                    path="/palette/:id"
+                    render={(rp) => (
+                      <div className="page">
+                        <Palette
+                          palette={this.findPalette(rp.match.params.id)}
+                        />
+                      </div>
+                    )}
+                  />
+                  <Route
+                    exact
+                    path="/palette/:paletteId/:colorId"
+                    render={(rp) => (
+                      <div className="page">
+                        <SingleColorPalette
+                          {...rp}
+                          colorId={rp.match.params.colorId}
+                          palette={this.findPalette(rp.match.params.paletteId)}
+                        />
+                      </div>
+                    )}
+                  />
+                  <Route render={() => <h1>Not found</h1>} />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        />
       </div>
     );
   }
